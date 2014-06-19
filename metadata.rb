22831268740a91994db5358af843c01270dd4661
chris <chris@locomote.com.au>
2014-06-19 14:37:26 +1000

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email 'devs@locomote.com'
 license          'BSD'
 description      'Chef LWRP to manage syschecks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
 %w{ubuntu debian redhat centos gentoo}.each { |os| supports os }
